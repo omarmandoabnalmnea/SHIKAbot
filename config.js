4cd0d9adfa771d5169a7e786a64b8e2a9aa0ce2f
SHIKA7777 <166670332+SHIKA7777@users.noreply.github.com>
2024-05-09 05:51:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,37 +7,37 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "994406837340"
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272']
+  ['201005603546', '👑 Mystic - Creador 👑', true],
+  ['201005603546', '💫 Mystic - Collaborator 2 💫', true],
+  ['201005603546', '💫 Mystic - Collaborator 3 💫', true],
+  ['201005603546', '💫 Mystic - Collaborator 4 💫', true],
+  ['201005603546', '💫 Mystic - Collaborator 5 💫', true],
+  ['201005603546', '💫 Mystic - Collaborator 6 💫', true],
+  ['201005603546', '💫 Mystic - Rey Endymion 💫', false],
+  ['201005603546', '💫 Mystic - Collaborator 8 💫', true],
+  ['201005603546', '💫 Mystic - By Alba070503 💫', true],
+  ['201005603546','💫 Mystic - Tester Fabri115💫', true],
+  ['201005603546'],
+  ['201005603546'],
+  ['201005603546'],
+  ['201005603546'],
+  ['201005603546'], 
+  ['201005603546'],  
+  ['201005603546'],
+  ['201005603546'],
+  ['201005603546'],
+  ['201005603546'], 
+  ['201005603546'], 
+  ['201005603546'],
+  ['201005603546']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['201005603546'];
+global.prems = ['201005603546'];
 
 global.packname = 'Sticker';
 global.author = 'The Mystic - Bot';
